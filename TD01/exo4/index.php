8d4ef2fc8240bcae63bf0ef412c0fdecd22aa18e
Laurine <30570842+LafLaurine@users.noreply.github.com>
2019-02-28 17:29:09 +0100

    Add files via upload
    index.php est sale. movie_list.php + lisible
    
    Pas inclusion de movie_list dans l'index car sinon gros bazard, il faut directement regarder dans /exo4/movie_list.php

--- TD01/exo4/index.php
+++ TD01/exo4/index.php
@@ -7,63 +7,60 @@
 </head>
 <body>
 
-    <h1>MOVIES</h1>
+    <h1 id="title">MOVIES</h1>
+    <div id="grid">
     <?php 
     require_once $_SERVER['DOCUMENT_ROOT']. '/PHP/TD01/exo4/data/data_movies.php';
-    //require_once $_SERVER['DOCUMENT_ROOT']. '/PHP/TD01/exo4/movie_list.php';
-
-
-    function is_comedy($var)
-    {
-        if($var == "comedy")
-        {
-            echo "LOL";
-        }
-    }
 
     foreach($movies as $movie){
-        echo $movie['title'];
+        echo "<h3 class='movie'>".$movie['title']."</h3>";
 
         if(is_string($movie['genre'])){
             $genre = $movie['genre'];
-            echo $genre;
-            is_comedy($genre);
+            echo "<ul>";
+            echo "<li class='movie-list'>".$genre."</li>";
+            if($genre == "Comedy")
+            {
+                echo "<li>LOL C 1 KOMEDIE</li>";
+            }
         }
        else if(is_array($movie['genre'])){
             if(isset($movie['genre'])){
+                echo "<ul>";
                 foreach ($movie['genre'] as $genre){
-                    echo $genre;
-                    is_comedy($genre);
+                    echo "<li class='movie-list'>".$genre."</li>";
+                    if($genre == "Comedy")
+                    {
+                        echo "<li>LOL C 1 KOMEDIE</li>";
+                    }
                 }
         }
     }
 
         $date = date($movie['date']);
-        echo $date;
+        echo "<li class='movie-list'>Date : ".$date."</li>";
         $dates = explode("-", $date);  
         $month = $dates[1];
         if($month == "02")
         {
-            echo "FEVRIER";
+            echo "<li>LOL C EN FEVRIER</li>";
         }
 
 
         if(is_string($movie['director'])){
-            echo $movie['director'];
+            echo "<li> Director : ". $movie['director'] ."</li>";
         }
        else if(is_array($movie['director'])){
             if(isset($movie['director'])){
                 foreach ($movie['director'] as $director){
-                    echo $director;
+                    echo "<li> Director : ". $director ."</li></br>";
                 }
         }
     
     }
-    }
-
-   // render_movie_list($movies, "Drama","04");
-
-
+    echo"</ul>";
+}
     ?>
+    </div>
 </body>
 </html>
--- TD01/exo4/movie_list.php
+++ TD01/exo4/movie_list.php
@@ -12,40 +12,47 @@ function render_movie_list ($movies, $genre, $month) {
 
       if($months == $month) {
         $current_month = "green";
+        $emoji ="<i class='em em-arrow_left'></i>";
       }
 
       else {
         $current_month = "";
+        $emoji = "";
       }
-
+      
       if(is_string($movie['genre'])) {
         if($movie['genre'] = $genre) {
           $current_genre = "red";
+          $emoji ="<i class='em em-arrow_left'></i>";
         }
         else {
           $current_genre = "";
+          $emoji = "";
         }
       }
       
       else if(is_array($movie['genre'])) {
             if(in_array($genre,$movie['genre'])) {
               $current_genre = "red";
+              $emoji ="<i class='em em-arrow_left'></i>";
             }
             else {
               $current_genre = "";
+              $emoji = "";
             }
       }
 
-      echo "<h3 class='movie'>".$movie['title']." <span class=".$current_month.">(".$date.")</span></h3>";
+      echo "<h3 class='movie'>".$movie['title']." <span class=".$current_month.">(".$date.")" ." ". $emoji . "</span></h3>";
+      echo "<ul>";
 
       if(is_array($movie["genre"])) {
-        echo "<li class='movie-list'>Genre : <span class=".$current_genre.">".implode(" ",$movie['genre'])."</span></li>";
+        echo "<li class='movie-list'>Genre : <span class=".$current_genre.">".implode(" ",$movie['genre']). " " . $emoji . "</span></li>";
       }
   
       else {
-        echo "<li class='movie-list'>Genre : <span class=".$current_genre.">".$movie['genre']."</span></li>";
+        echo "<li class='movie-list'>Genre : <span class=".$current_genre.">".$movie['genre']. " " . $emoji. "</span></li>";
       }
-
+      echo "</ul>";
 
 
 
@@ -63,10 +70,11 @@ function render_movie_list ($movies, $genre, $month) {
     <meta charset="UTF-8">
     <title>Le Fil ROUGE</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
   </head>
   <body>
 
-    <h1 id="title">Movies </h1>
+    <h1 id="title"><i class="em em-film_frames"></i> Movies <i class="em em-film_frames"></i></h1>
     <div id="grid">
       <?php 
       render_movie_list($movies, "Drama","04");
